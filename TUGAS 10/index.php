0e3a7bb2bcdff87949bc1f6631eae105dfcb089e
rizkiset <mrizkisetiobudi@saintek.unipdu.ac.id>
2024-12-05 01:10:42 +0700

    UP

---
+++ TUGAS 10/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" connect="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sistem Informasi Sekolah</h1>
+    <h2>SMK PGRI 2 JOMBANG </h2>
+    <a href="data_siswa.php">Daftar Siswa</a><br>
+    <a href="tambah_siswa.php">Pendafartan</a>
+</body>
+</html>
